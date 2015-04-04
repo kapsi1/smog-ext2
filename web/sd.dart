93ae370b79a8835b13372c46677cb81b4a9fd156
kapsi <kapsi@o2.pl>
2015-04-04 12:38:29 +0200

    Remove unneeded tabs  permission in manifest, style fix, changeLocation() changes speed dial url, show "b/d" if data older than 3 hours.

--- web/sd.dart
+++ web/sd.dart
@@ -8,6 +8,10 @@ import 'dart:async';
 import 'dart:js';
 
 final shownPollutants = ['SO2', 'NO2', 'PM10', 'PM2.5'];
+//for linking to monitoring.krakow.pios.gov.pl
+final locationMapping = {'Tarnów': 4, 'Skawina': 5, "Kraków, Al. Krasińskiego": 6,
+  "Kraków, ul. Bulwarowa": 7, 'Nowy Sącz': 8, 'Zakopane': 9, 'Olkusz': 10,
+  'Trzebinia': 11, 'Szymbark': 12, 'Szarów': 15, "Kraków, ul. Bujaka": 16};
 //String serverUrl = 'http://localhost:8080';
 String serverUrl = 'https://smog-server.herokuapp.com';
 Map<String, Pollutant> displayedPollutants = {};
@@ -32,25 +36,34 @@ class Pollutant {
 
   update(DateTime t, double v) {
     String padZeroes(int n) => n > 9 ? n.toString() : '0' + n.toString();
-    time = t.toLocal();
+    t = t.toLocal();
     value = v;
-    domEl.querySelector('footer').text = '${padZeroes(time.hour)}:${padZeroes(time.minute)}';
-
-    int percentVal = (value / norms[shortLabel] * 100).round();
-    domEl.querySelector('section').text = percentVal.toString() + '%';
-
-    if (percentVal >= 100) {
-      domEl.className = 'over-norm';
-    } else if (percentVal >= 75) {
-      domEl.className = 'warning';
-    } else {
+    if (time != null && t.difference(time).inHours.abs() > 3) {
+      domEl.querySelector('section').text = 'b/d';
       domEl.className = 'in-norm';
+    } else {
+      int percentVal = (value / norms[shortLabel] * 100).round();
+      domEl.querySelector('section').text = percentVal.toString() + '%';
+
+      if (percentVal >= 100) {
+        domEl.className = 'over-norm';
+      } else if (percentVal >= 75) {
+        domEl.className = 'warning';
+      } else {
+        domEl.className = 'in-norm';
+      }
     }
+    time = t.toLocal();
+    domEl.querySelector('footer').text = '${padZeroes(time.hour)}:${padZeroes(time.minute)}';
   }
 }
 
 changeLocation(String location) {
-  var options = new JsObject.jsify({'title': 'Smog: $location'});
+  var options = new JsObject.jsify({
+                                     'title': 'Smog: $location',
+                                     'url': 'http://monitoring.krakow.pios.gov.pl/dane-pomiarowe/automatyczne/stacja/'
+                                     + '${locationMapping[location]}/parametry/wszystkie'
+                                   });
   context['opr']['speeddial'].callMethod('update', [options]);
 
   querySelector('main').nodes.clear();
@@ -63,7 +76,7 @@ changeLocation(String location) {
     Map pollutants = locationData['pollutants'];
     List sorted = new List.from(pollutants.keys);
     sorted.sort();
-    sorted.forEach((shortName) {
+    for (String shortName in sorted) {
       if (!shownPollutants.contains(shortName)) return;
       Map lastValue = pollutants[shortName]['lastValue'];
       Pollutant p = new Pollutant(shortName);
@@ -71,8 +84,7 @@ changeLocation(String location) {
       double v = lastValue['value'];
       p.update(d, v);
       displayedPollutants[p.shortLabel] = p;
-    });
-
+    }
     if (timer != null) timer.cancel();
     timer = new Timer.periodic(new Duration(minutes: 5), (Timer timer) {
       updatePollutantValues(location);
@@ -82,16 +94,18 @@ changeLocation(String location) {
 }
 
 updatePollutantValues(String location) {
+  print('updatePollutantValues $location');
   HttpRequest httpRequest = new HttpRequest();
   httpRequest
-    ..open('GET', '$serverUrl/locations/$location')
+    ..open('GET', serverUrl + "/location/" + "?val=" + Uri.encodeComponent(location))
     ..onLoadEnd.listen((e) {
     Map locationData = JSON.decode(httpRequest.responseText);
     Map pollutants = locationData['pollutants'];
-    displayedPollutants.keys.forEach((String pollutantName) {
+
+    for (String pollutantName in displayedPollutants.keys) {
       var p = pollutants[pollutantName];
       displayedPollutants[pollutantName].update(DateTime.parse(p['lastValue']['dateTime']), p['lastValue']['value']);
-    });
+    }
   })
     ..send();
 }
